da53cdf94bfffee9cb2b74c9adcc1f24ae5dc06e
yusrilmustofa <53527287+yusrilmustofa@users.noreply.github.com>
2019-11-14 10:06:25 +0700

    Add files via upload

--- Tes.java
+++ Tes.java
@@ -10,7 +10,12 @@ package Latihan2;
  * @author yusri
  */
 public class Tes {
-    public static void main(String []srgs){
-      
+
+    public static void main(String[] Rjr) {
+        Line RJR = new Line(5, 10, 10, 20);
+        System.out.println(RJR.getLength());
+        System.out.println(RJR.isGreater(RJR, RJR));
+        System.out.println(RJR.isLess(RJR, RJR));
+        System.out.println(RJR.isEqual(RJR, RJR));
     }
 }
